0ddab0b8430c8749f68261cf91ff77c777743661
Michael Bilodeau <mbilodeau6@hotmail.com>
2020-09-13 08:27:28 -0700

    Convert to Card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { DataService } from './data.service';
 import { Game } from './game';
+import { Player } from './player';
+import { Card, Suit, Rank } from './card';
 
 @Component({
   selector: 'app-root',
@@ -15,8 +17,28 @@ export class AppComponent {
     this.gameState = new Game();
   }
 
+  // TODO: The version of Card from HTTP.get is not a real card. Why not?
+  // Do the conversion manually until I figure it out.
+  setGameState(gameState: Game) {
+    let newGameState: Game = new Game();
+    newGameState.players = [];
+    for(let player of gameState.players) {
+      let newPlayer = new Player(player.name);
+
+      for(let card of player.hand) {
+        // TODO: Looks like suit and rank aren't real either
+        let newCard = new Card(Suit[card.suit], Rank[card.rank]);
+        newPlayer.hand.push(newCard);
+      }
+
+      newGameState.players.push(newPlayer);
+    }
+
+    this.gameState = newGameState;
+  }
+
   getGameState(): void {
-    this.dataService.getGameState().subscribe(gameState => this.gameState = gameState);
+    this.dataService.getGameState().subscribe(gameState => this.setGameState(gameState));
   }
 
   ngOnInit() {
